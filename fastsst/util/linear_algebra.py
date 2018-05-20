ce6803e9350aabd2bface3a478be9b71babaf1ef
apple <tmgf.15.su@gmail.com>
2018-05-20 15:11:34 +0900

    pep8

--- fastsst/util/linear_algebra.py
+++ fastsst/util/linear_algebra.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
+"""Utility funcs for linear algebra."""
+
 from numba import jit
 import numpy as np
 from numpy.linalg import norm
 
+
 @jit(nopython=True)
-def power_method(A,x0,n_iter=1):
-    """compute the first singular components by power method
-    """
+def power_method(A, x0, n_iter=1):
+    """Compute the first singular components by power method."""
     for i in range(n_iter):
         x0 = A.T @ A @ x0
 
@@ -14,17 +16,17 @@ def power_method(A,x0,n_iter=1):
     s = norm(A @ v)
     u = A @ v / s
 
-    return u,s,v
+    return u, s, v
+
 
 @jit(nopython=True)
-def lanczos(C,a,s):
-    """run lanczos algorithm
-    """
+def lanczos(C, a, s):
+    """Perform lanczos algorithm."""
     # initialization
     r = np.copy(a)
-    a_pre = np.zeros_like(a,dtype=np.float64)
+    a_pre = np.zeros_like(a, dtype=np.float64)
     beta_pre = 1
-    T = np.zeros((s,s))
+    T = np.zeros((s, s))
 
     for j in range(s):
         a_post = r / beta_pre
@@ -32,10 +34,10 @@ def lanczos(C,a,s):
         r = C @ a_post - alpha*a_post - beta_pre*a_pre
         beta_post = norm(r)
 
-        T[j,j] = alpha
+        T[j, j] = alpha
         if j - 1 >= 0:
-            T[j,j-1] = beta_pre
-            T[j-1,j] = beta_pre
+            T[j, j-1] = beta_pre
+            T[j-1, j] = beta_pre
 
         # update
         a_pre = a_post
@@ -43,10 +45,10 @@ def lanczos(C,a,s):
 
     return T
 
+
 @jit(nopython=True)
 def eig_tridiag(T):
-    """compute eigen value decomposition for tridiag matrix
-    """
+    """Compute eigen value decomposition for tridiag matrix."""
     # TODO: efficient implementation
     # ------------------------------------------------------
     # Is it really necessary to implement fast eig computation ?
@@ -54,6 +56,6 @@ def eig_tridiag(T):
     # larger than n_components. Therefore fast implementation such as
     # QL algorithm may not provide computational cost benefit in total.
     # ------------------------------------------------------
-    u,s,_ = np.linalg.svd(T)
+    u, s, _ = np.linalg.svd(T)
     # NOTE: return value must be ordered
-    return u,s
+    return u, s
